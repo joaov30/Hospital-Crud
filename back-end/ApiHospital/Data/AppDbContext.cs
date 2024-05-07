459dc070bbb622a5947056b2ca23adb8da7df454
Joao Victor Baptista <joaovictordacostabaptista@hotmail.com>
2024-05-07 19:46:34 -0300

    Criando uma injeção de dependencia em Program.cs, e instalando o Pacote EFcore Design

--- back-end/ApiHospital/Data/AppDbContext.cs
+++ back-end/ApiHospital/Data/AppDbContext.cs
@@ -1,7 +1,17 @@
-﻿namespace ApiHospital.Data
+﻿using ApiHospital.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace ApiHospital.Data
 {
-    public class AppDbContext
+    public class AppDbContext : DbContext
     {
+       private DbSet<Paciente> pacientes { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Data Source=BancoPacientes.sqlite");
+            base.OnConfiguring(optionsBuilder);
+        }
 
     }
 }
