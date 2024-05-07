cd21f913dabe0efea7b1307ae4a23836f686a0ac
Joao Victor Baptista <joaovictordacostabaptista@hotmail.com>
2024-05-07 16:20:37 -0300

    Criando o Modelo da Tabela

---
+++ back-end/ApiHospital/Model/Paciente.cs
@@ -0,0 +1,19 @@
+﻿namespace ApiHospital.Model
+{
+    public class Paciente
+    {
+        public Guid Id { get; init; }
+
+        public string Nome { get; init; }
+
+        public string DataNascimento { get; set; }
+
+        public string Genero { get; set; }
+
+        public string Endereco { get; set; }
+
+        public string Telefone { get; set; }
+
+        public string Email { get; set; }
+    }
+}
---
+++ back-end/ApiHospital/Pacientes/PacienteRotas.cs
@@ -0,0 +1,10 @@
+﻿namespace ApiHospital.Pacientes
+{
+    public static class PacienteRotas
+    {
+        public static void AddPacientesRotas(this WebApplication app)
+        {
+            app.MapGet("pacientes", () => "Lista de Pacientes: ");
+        }
+    }
+}
